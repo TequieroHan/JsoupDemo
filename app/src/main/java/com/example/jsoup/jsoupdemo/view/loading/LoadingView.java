5dfa7c9d5f032701a73b9e5a56a964050f5f8099
TequieroHan <maoyyong@163.com>
2017-11-10 10:21:03 +0800

    Loading

---
+++ app/src/main/java/com/example/jsoup/jsoupdemo/view/loading/LoadingView.java
@@ -0,0 +1,38 @@
+package com.example.jsoup.jsoupdemo.view.loading;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.example.jsoup.jsoupdemo.R;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class LoadingView extends FrameLayout {
+
+    public LoadingView(@NonNull Context context) {
+        this(context,null);
+    }
+
+    public LoadingView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public LoadingView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+    }
+}
